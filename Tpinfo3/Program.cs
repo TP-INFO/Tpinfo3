83e9ed9577bdf3276ac83e076e74ad2f14b8680e
Clément Flandin <clemflandin@gmail.com>
2017-04-03 11:10:06 +0200

    Update Program.cs

--- Tpinfo3/Program.cs
+++ Tpinfo3/Program.cs
@@ -10,19 +10,24 @@ namespace _2017_03_22_TP3
     {
         static void Main(string[] args)
         {
+            //Déclaration du tableau de char 20x20
             char[,] tab = new char[20, 20];
+            //Remplissage 1ère ligne
             for (int i = 0; i < 20; i++)
             {
                 tab[0, i] = '#';
             }
+            //Remplissage 1ère colonne
             for (int j = 0; j < 20; j++)
             {
                 tab[j, 0] = '#';
             }
+            //Remplissage dernière ligne
             for (int k = 0; k < 20; k++)
             {
                 tab[19, k] = '#';
             }
+            //Parcours le tableau pour afficher le 'C'
             for (int l = 0; l < 20; l++)
             {
                 for (int m = 0; m < 20; m++)
